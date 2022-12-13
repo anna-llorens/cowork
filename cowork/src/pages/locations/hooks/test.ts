9d1e61a69205332db0632b0d7fb4c60f18fdad0e
Anna Llorens <anna.llorens@xing.com>
2022-12-13 14:45:40 +0100

    grrr

--- cowork/src/pages/locations/hooks/test.ts
+++ cowork/src/pages/locations/hooks/test.ts
@@ -1,1 +1,127 @@
-//
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
