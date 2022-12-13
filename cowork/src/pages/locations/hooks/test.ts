2c81c0169fabd2d110b034cec8b08f4485f4d053
Anna Llorens <anna.llorens@xing.com>
2022-12-13 14:45:00 +0100

    grrr

--- cowork/src/pages/locations/hooks/test.ts
+++ cowork/src/pages/locations/hooks/test.ts
@@ -13,3 +13,163 @@ type User = {
   name: any;
   casa: Casa;
 };
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
+
+console.log("hello");
+
+interface CompanyInfo extends Casa {
+  location?: string;
+  name?: string;
+  id?: string;
+  salary?: string;
+  kununoInfo?: string;
+}
+
+type User = {
+  id: any;
+  name: any;
+  casa: Casa;
+};
