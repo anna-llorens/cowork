9a3c177099a6b2e0d4c149865bd393b2f6fbf64d
Anna Llorens <anna.llorens@xing.com>
2022-12-13 14:43:50 +0100

    Locations

--- cowork/src/pages/locations/hooks/test.ts
+++ cowork/src/pages/locations/hooks/test.ts
@@ -13,30 +13,3 @@ type User = {
   name: any;
   casa: Casa;
 };
-
-type Casa = {
-  location?: string;
-  name?: string;
-  id?: string;
-  salary?: string;
-
-  hh?: string;
-  pp?: string;
-};
-
-const user: User = {
-  id: 666,
-  name: "bytefer",
-  casa: { hh: "", pp: "hey pp", location: "aa" },
-};
-console.log("holaa");
-
-const test1 = (a) => {
-  let { casa, ...rest }: User = user;
-  console.log(casa, "location");
-};
-
-test1();
-
-const keys = Object.keys(user);
-console.log(keys);
