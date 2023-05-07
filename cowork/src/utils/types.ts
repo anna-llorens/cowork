4c7920f0519855bd714d6a13a752b32adda092b9
Anna Llorens <annallorens_12@hotmail.com>
2023-05-07 22:04:06 +0200

    Added app tokens

---
+++ cowork/src/utils/types.ts
@@ -0,0 +1,33 @@
+export enum Routes {
+  Register = "register",
+  Locations = "locations",
+  Login = "login",
+}
+
+export enum RoutePath {
+  locations = "/locations/",
+
+}
+
+type Address = {
+  city?: string;
+  postalCode?: string;
+  street?: string;
+  country: string;
+};
+
+export type Person = {
+  id?: string;
+  name: string;
+  surname: string;
+  email?: string;
+  number?: string;
+};
+
+export type Cowork = {
+  id?: string;
+  companyName: string;
+  web?: string;
+  address: Address;
+  contact: Person;
+};
